a800cd89f5d40667960d1020e61ed46131eaf05a
Defragster <Defragster@users.noreply.github.com>
2023-07-12 13:29:01 -0700

    tests pushed

--- WFIon1062_Simple/WFIon1062_Simple.ino
+++ WFIon1062_Simple/WFIon1062_Simple.ino
@@ -1,19 +1,17 @@
-void callback() {
+void RTCcallback() {
   SNVS_HPSR |= 0b11;               // reset interrupt flag
   digitalToggleFast(LED_BUILTIN);
   asm("dsb");                      // wait until flag is synced over the busses to prevent double calls of the isr
 }
 
-valPIF given divisor of 32Khz of 15-0 = Hz: 15=1, 14=2, 13=4, ..., 3=4096, 2=8192, 1=16384, 0=32772
+// valPIF given divisor of 32Khz of 15-0 = Hz: 15=1, 14=2, 13=4, ..., 3=4096, 2=8192, 1=16384, 0=32772
 void setupRTCInterrupt( uint8_t valPIF ) {
-  // disable periodic interrupt
-  SNVS_HPCR &= ~SNVS_HPCR_PI_EN;
+  SNVS_HPCR &= ~SNVS_HPCR_PI_EN; // disable periodic interrupt
   while ((SNVS_HPCR & SNVS_HPCR_PI_EN)) {}  //The RTC is a slow peripheral, need to spin until PI_EN is reset...
   SNVS_HPCR = SNVS_HPCR_PI_FREQ( valPIF );
-  // enable periodic interrupt
-  SNVS_HPCR |= SNVS_HPCR_PI_EN;
+  SNVS_HPCR |= SNVS_HPCR_PI_EN; // enable periodic interrupt
   while (!(SNVS_HPCR & SNVS_HPCR_PI_EN)) {}  // spin until PI_EN is set...
-  attachInterruptVector(IRQ_SNVS_IRQ, callback);
+  attachInterruptVector(IRQ_SNVS_IRQ, RTCcallback);
   NVIC_SET_PRIORITY(IRQ_SNVS_IRQ, 16);  // some priority 0-255 (low number = high priority)
   NVIC_ENABLE_IRQ(IRQ_SNVS_IRQ);
 }
